68975c60ac3063149fa45d757f6bd2078f11ae39
Juan Martin Nazarian <jmnazarian@gmail.com>
2023-05-30 16:04:48 -0300

    Agregar enemigos a juego y metodo para verificar si el juego ha terminado

--- src/main/java/edu/fiuba/algo3/Juego.java
+++ src/main/java/edu/fiuba/algo3/Juego.java
@@ -1,14 +1,20 @@
 package edu.fiuba.algo3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.fiuba.algo3.*;
 
 public class Juego {
     private Mapa mapa;
     private Turno turno;
     private Jugador jugador;
+    private List<Enemigo> enemigos;
 
     public Juego() {
+        this.enemigos = new ArrayList<Enemigo>();
     }
+
     public Juego(Jugador jugador, Mapa mapa) {
         this.jugador = jugador;
         this.mapa = mapa;
@@ -20,4 +26,12 @@ public class Juego {
     }
 
     public Jugador obtenerJugador() { return this.jugador; };
+
+    public void agregarEnemigo(Enemigo enemigo){
+        enemigos.add(enemigo);
+    }
+
+    public boolean juegoTerminado(){
+        return enemigos.size() == 0;
+    }
 }
