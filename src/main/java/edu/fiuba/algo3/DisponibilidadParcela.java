46b8ab09e7be1eb54147ca1c5677408e48a2ce05
joacomi <joaquinmiranda.i@hotmail.com>
2023-06-01 04:52:49 -0300

    Strategy pattern DisponibilidadParcela

--- src/main/java/edu/fiuba/algo3/DisponibilidadParcela.java
+++ src/main/java/edu/fiuba/algo3/DisponibilidadParcela.java
@@ -4,5 +4,6 @@ import edu.fiuba.algo3.exceptions.NoDisponibleParaConstruirException;
 
 public interface DisponibilidadParcela {
     void agregar(Defensa defensa, Parcela parcela) throws NoDisponibleParaConstruirException;
+    void verSiEstaDisponible() throws NoDisponibleParaConstruirException;
 }
 
