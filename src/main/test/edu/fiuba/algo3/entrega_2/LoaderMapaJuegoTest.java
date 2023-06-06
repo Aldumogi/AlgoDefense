8ce3752e7728aea2e7dae15c78e649a510615815
amogilewski <amogilewski@fi.uba.ar>
2023-06-06 01:11:41 -0300

    caso 16 casi terminado (falta refactor)

---
+++ src/main/test/edu/fiuba/algo3/entrega_2/LoaderMapaJuegoTest.java
@@ -0,0 +1,24 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.LoaderEnemigosJuego;
+import edu.fiuba.algo3.LoaderMapaJuego;
+import edu.fiuba.algo3.Parcela;
+import edu.fiuba.algo3.Turno;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoaderMapaJuegoTest {
+    @Test
+    public void readJSONTest() throws IOException, ParseException {
+
+        Map<Integer, ArrayList<Parcela>> mapa = LoaderMapaJuego.recuperarMapa();
+
+        assert(mapa.size() > 0);
+    }
+}
---
+++ src/main/test/edu/fiuba/algo3/entrega_2/MapaTest.java
@@ -0,0 +1,23 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.Coordenadas;
+import edu.fiuba.algo3.Mapa;
+import edu.fiuba.algo3.Parcela;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class MapaTest {
+
+    @Test
+    public void elMapaSeCreaCorrectamente() throws IOException, ParseException {
+        Mapa mapa = new Mapa();
+
+        for(int i = 1; i <= mapa.obtenerCantidadDeFilas(); i++) {
+            for(int j = 0; j < mapa.obtenerCantidadDeColumnas(); j++) {
+                assert(mapa.obtenerCelda(new Coordenadas(i, j)) instanceof Parcela);
+            }
+        }
+    }
+}
