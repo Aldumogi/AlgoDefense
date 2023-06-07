5d3b4a357171681fbf49894fbf7203997af17023
DiegoDiGrazia <diegodigrazia.3@gmail.com>
2023-06-07 17:36:13 -0300

    Agregue los test del mapa, la funcion devolver siguiente parcela con sus respectivos test. Tuve que agregar unos equals para poder resolverlo

--- src/main/test/edu/fiuba/algo3/entrega_2/MapaTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/MapaTest.java
@@ -7,9 +7,13 @@ import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class MapaTest {
 
@@ -23,4 +27,60 @@ public class MapaTest {
             }
         }
     }
+    @Test
+    public void esPorAcaTestBasico() throws IOException, ParseException {    
+
+        Mapa unMapa = new Mapa();
+        Coordenadas coordenadaActual = new Coordenadas(3, 2);
+        List<Coordenadas> listaDeVisitados = new ArrayList<>();
+        listaDeVisitados.add(coordenadaActual);
+        Coordenadas cordenadaAChequearVerdadera = new Coordenadas(4,2);
+        Coordenadas cordenadaAChequearFalse = new Coordenadas(2,2);
+
+        Boolean esPorAca = unMapa.esPorAca(cordenadaAChequearVerdadera, listaDeVisitados);
+
+        assertFalse(unMapa.esPorAca(cordenadaAChequearFalse, listaDeVisitados));
+
+        assertTrue(esPorAca);
+
+    }
+    @Test
+    public void esPorAcaSaliendoDesdeLargadaTest() throws IOException, ParseException {    
+
+        Mapa unMapa = new Mapa();
+        Coordenadas coordenadaActual = new Coordenadas(1, 2);
+        List<Coordenadas> listaDeVisitados = new ArrayList<>();
+        listaDeVisitados.add(coordenadaActual);
+        Coordenadas cordenadaAChequearVerdadera = new Coordenadas(2,2);
+        Coordenadas cordenadaAChequearFalse = new Coordenadas(2,3);
+
+        Boolean esPorAca = unMapa.esPorAca(cordenadaAChequearVerdadera, listaDeVisitados);
+
+        assertFalse(unMapa.esPorAca(cordenadaAChequearFalse, listaDeVisitados));
+
+        assertTrue(esPorAca);
+    }
+    
+    @Test
+    public void devuelvoLaSiguientePasarelaMasCercanaALaMetaCorrectamente() throws IOException, ParseException {    
+
+        Mapa unMapa = new Mapa();
+        Coordenadas coordenadaActual = new Coordenadas(4, 2);
+        Coordenadas cordenadaEsperada = new Coordenadas(5, 2);
+
+        Coordenadas ATestear = unMapa.devolverSiguientePasarela_(coordenadaActual);
+        assertTrue(ATestear.equals(cordenadaEsperada));
+    }
+
+    @Test
+    public void devuelvoLaSiguientesNPasarelasMasAdelanteCorrectamente() throws IOException, ParseException {    
+
+        Mapa unMapa = new Mapa();
+        Coordenadas coordenadaActual = new Coordenadas(3, 2);
+         Coordenadas cordenadaEsperada = new Coordenadas(5, 2);
+
+         Coordenadas ATestear = unMapa.devolverSiguientePasarela(coordenadaActual, 2);
+        assertTrue(ATestear.equals(cordenadaEsperada));
+    }
+
 }
