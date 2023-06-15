3ee0d4e55c55f5fa342d73d9d0c0dad87e6509bb
joacomi <joaquinmiranda.i@hotmail.com>
2023-06-15 01:33:14 -0300

    Fix logger

--- src/main/java/edu/fiuba/algo3/modelo/LoggerManager.java
+++ src/main/java/edu/fiuba/algo3/modelo/LoggerManager.java
@@ -1,8 +1,13 @@
 package edu.fiuba.algo3.modelo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class LoggerManager {
+    static {
+        String configLocation = "src/main/java/edu/fiuba/algo3/resources/log4j2.xml";
+        Configurator.initialize(null, configLocation);
+    }
     public static final Logger logger = LogManager.getLogger(LoggerManager.class);
 }
 
